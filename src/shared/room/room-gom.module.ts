04e55befc39be020e7e7ffda04378c6f25a669b2
Devo Astroman <devo.astroman@gmail.com>
2024-08-30 13:23:52 +0200

    clean comments

--- src/shared/room/room-gom.module.ts
+++ src/shared/room/room-gom.module.ts
@@ -9,7 +9,6 @@ import { Loose } from "shared/loose/loose.module";
 
 export class RoomGom extends MyMaid {
 	private root: Instance;
-	//private connection!: RBXScriptConnection;
 	private prematch!: Prematch;
 	private match!: Match;
 	private win!: Win;
@@ -25,59 +24,26 @@ export class RoomGom extends MyMaid {
 	}
 
 	getPrematchFolder() {
-		/* const prematchFolder = this.root.FindFirstChild("Prematch", true);
-		if (!prematchFolder) {
-			print("Warning, not found ", prematchFolder);
-		}
-		return prematchFolder; */
-
 		const preMatchFolder = findElement<Folder>(this.root, "Prematch");
 		return preMatchFolder;
 	}
 
 	getMatchFolder() {
-		/* const matchFolder = this.root.FindFirstChild("Match", true);
-		if (!matchFolder) {
-			print("Warning, not found ", matchFolder);
-		} */
-
 		const matchFolder = findElement<Folder>(this.root, "Match");
 		return matchFolder;
 	}
 
 	getResetEvent() {
-		/* const event = this.root.FindFirstChild("ResetEvent");
-		if (!event) {
-			print("Warning, not found ", "ResetEvent");
-		}
-		return event as BindableEvent; */
 		const event = findElement<BindableEvent>(this.root, "ResetEvent");
 		return event;
 	}
 
 	getPhaseFinishedEvent() {
-		/* const event = this.root.FindFirstChild("PhaseFinishedEvent");
-		if (!event) {
-			print("Warning, not found ", event);
-		}
-		return event as BindableEvent; */
-
 		const event = findElement<BindableEvent>(this.root, "PhaseFinishedEvent");
 		return event;
 	}
-	/* 
-	onPhaseFinished(cb: (id: ROOM_PHASE, players: Model[]) => void) {
-		const event = this.getPhaseFinishedEvent() as BindableEvent;
-		this.connection = event.Event.Connect(cb);
-	} */
 
 	getWinFolder() {
-		/* const winFolder = this.root.FindFirstChild("Win") as Folder;
-		if (!winFolder) {
-			print("Warning, not found ", "Win");
-		}
-		return winFolder; */
-
 		const winFolder = <Folder>findElement(this.root, "Win");
 		return winFolder;
 	}
@@ -94,8 +60,6 @@ export class RoomGom extends MyMaid {
 	}
 
 	onPrematchFinished(cb: (players: Model[]) => void) {
-		//this.prematch.getFinishedEvent()
-
 		this.maidConnection(this.prematch.getFinishedEvent().Event, cb);
 	}
 	initPrematch() {
@@ -103,7 +67,6 @@ export class RoomGom extends MyMaid {
 	}
 
 	createMatch(roomPhase: ROOM_PHASE, stores: Stores) {
-		//change stores to matchStores
 		const matchFolder = this.getMatchFolder();
 		this.match = new Match(roomPhase, matchFolder, stores);
 		return this.match;
@@ -146,8 +109,6 @@ export class RoomGom extends MyMaid {
 	}
 
 	prepareMaid(): void {
-		//this.addListToMaid([this.connection]);
-
 		this.addListToMaid([this.prematch, this.match, this.win, this.loose]);
 	}
 }
--- src/shared/room/room.module.ts
+++ src/shared/room/room.module.ts
@@ -1,29 +1,16 @@
 import { Observer } from "@rbxts/observer";
 import { MATCH_FINISH, ROOM_PHASE } from "shared/constants.module";
-import { Prematch } from "shared/prematch/prematch.module";
 import { Stores } from "shared/stores/stores.module";
 import { RoomGom } from "./room-gom.module";
-import { Match } from "shared/match/match.module";
 import { MyMaid } from "shared/maid/my-maid.module";
 import { getUserIdFromPlayerCharacter } from "shared/services/player-game-service.module";
-import { Loose } from "shared/loose/loose.module";
-import { Win } from "shared/win/win.module";
 
 export class Room extends MyMaid {
 	private stores: Stores;
 	private gom: RoomGom;
-
 	private room$: Observer<ROOM_PHASE>;
-
-	//private preMatch!: Prematch;
-	//private match!: Match;
-	//	private win!: Win;
-	//private loose!: Loose;
-
 	private resetEvent: BindableEvent;
 
-	private connection!: RBXScriptConnection;
-	private connections: RBXScriptConnection[] = [];
 	constructor(instance: Instance) {
 		super();
 		print("- Room -");
@@ -32,8 +19,6 @@ export class Room extends MyMaid {
 		this.room$ = this.stores.getRoomStoreState$();
 		this.resetEvent = this.gom.getResetEvent();
 
-		//const prematchFolder = this.gom.getPrematchFolder();
-		//this.preMatch = new Prematch(ROOM_PHASE.PREMATCH, prematchFolder);
 		this.gom.createPrematch(ROOM_PHASE.PREMATCH);
 		this.gom.onPrematchFinished((players) => {
 			//allow players go to match
@@ -41,62 +26,23 @@ export class Room extends MyMaid {
 			this.stores.setMatchStorePlayers(userIds); //These two should go to one action
 			this.stores.setRoomStoreState(ROOM_PHASE.MATCH); //These two should go to one action
 		});
-		/* const conn1 = this.preMatch.getFinishedEvent().Event.Connect((players) => {
-			//allow players go to match
-			const userIds = (players as Model[]).map((d) => getUserIdFromPlayerCharacter(d));
-			this.stores.setMatchStorePlayers(userIds); //These two should go to one action
-			this.stores.setRoomStoreState(ROOM_PHASE.MATCH); //These two should go to one action
-		}); */
 
-		////////////////////////
-		/* const matchFolder = this.gom.getMatchFolder(); */
-		/* if (matchFolder) { */
-		//this.match = new Match(ROOM_PHASE.MATCH, matchFolder, this.stores);
 		this.gom.createMatch(ROOM_PHASE.MATCH, this.stores);
 		this.gom.onMatchFinished((data: MATCH_FINISH) => {
-			print("Finished data ", data);
 			if (data === MATCH_FINISH.WIN) {
 				//congrats the player
 				this.stores.setRoomStoreState(ROOM_PHASE.WIN);
 			} else {
-				//congrats the player
+				//kill the players
 				this.stores.setRoomStoreState(ROOM_PHASE.LOOSE);
 			}
 		});
-
-		/* const conn2 = this.match.getPlayerInteractionEvent().Event.Connect((data) => {
-				print("Interaction data ", data);
-			});
-			this.connections.push(conn2); */
-
-		/* const conn3 = this.match.getFinishedEvent().Event.Connect((data: unknown) => {
-			print("Finished data ", data);
-			if (data === MATCH_FINISH.WIN) {
-				//congrats the player
-				this.stores.setRoomStoreState(ROOM_PHASE.WIN);
-			} else {
-				//congrats the player
-				this.stores.setRoomStoreState(ROOM_PHASE.LOOSE);
-			}
-		});
-		this.connections.push(conn3); */
-		/* } */
-
-		/* const winFolder = this.gom.getWinFolder();
-		this.win = new Win(0, this.stores, winFolder); */
 
 		this.gom.createWin(this.stores);
 		this.gom.onWinFinished(() => {
-			//this.stores.setRoomStoreState(ROOM_PHASE.PREMATCH);
 			this.resetEvent.Fire();
 		});
-		/* const conn4 = this.win.getFinishedEvent().Event.Connect(() => {
-			//this.stores.setRoomStoreState(ROOM_PHASE.PREMATCH);
-			this.resetEvent.Fire();
-		});
-		this.connections.push(conn4); */
 
-		//this.loose = new Loose(0, this.stores);
 		this.gom.createLoose(this.stores);
 		this.gom.onLooseFinished(() => {
 			this.resetEvent.Fire();
@@ -110,21 +56,11 @@ export class Room extends MyMaid {
 			if (phase !== undefined) {
 				if (phase === ROOM_PHASE.PREMATCH) {
 					this.gom.initPrematch();
-					//this.preMatch.init();
-					//preMatch.finished(()=>this.stores.setRoomStoreState(ROOM_PHASE.MATCH))
 				} else if (phase === ROOM_PHASE.MATCH) {
-					print("match");
-					//match.init
-					//this.match.init();
 					this.gom.initMatch();
 				} else if (phase === ROOM_PHASE.WIN) {
-					print("win");
-					//this.win.init();
 					this.gom.initWin();
 				} else if (phase === ROOM_PHASE.LOOSE) {
-					print("loose");
-					//this.loose.init();
-					//this.loose.killPlayers();
 					this.gom.initLoose();
 				}
 			}
@@ -138,7 +74,7 @@ export class Room extends MyMaid {
 	}
 
 	onResetEvent(cb: () => void) {
-		this.connection = this.resetEvent.Event.Connect(cb);
+		this.maidConnection(this.resetEvent.Event, cb);
 	}
 
 	getRootInstace() {
@@ -146,8 +82,7 @@ export class Room extends MyMaid {
 	}
 
 	prepareMaid() {
-		const list = [this.stores, this.gom, this.connection, ...this.connections];
-
+		const list = [this.stores, this.gom];
 		this.addListToMaid(list);
 	}
 }
