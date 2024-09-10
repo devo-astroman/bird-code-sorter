260f8795a599a744d387c5f8e5b8e93569c14613
Devo Astroman <devo.astroman@gmail.com>
2024-09-10 19:49:19 +0200

    run door cabinet animation in the client

--- src/shared/client/services/client-setup.module.ts
+++ src/shared/client/services/client-setup.module.ts
@@ -1,7 +1,46 @@
+import { findElement, findElementShallow } from "shared/services/gom-service.module";
 import { onClientReceiveMsg } from "shared/services/server-client-comm.module";
+
+const w = game.GetService("Workspace");
+const worldFolder = findElement(w, "World");
+const worldChildren = worldFolder.GetChildren();
+
+/* to test run animation for certain players
+const pS = game.GetService("Players");
+const currentUserId = pS.LocalPlayer.UserId; */
 
 export const setupAnimations = () => {
 	onClientReceiveMsg((msg) => {
 		print("MESSAGE IN CLIENT: ", msg);
+		const data = msg.data as {
+			roomId: number;
+			objectId: string;
+			userId: number;
+		};
+
+		const idRoom = data.roomId;
+
+		const roomFolder = worldChildren
+			.filter((child) => {
+				const isFolder = child.IsA("Folder");
+				return isFolder;
+			})
+			.find((child) => {
+				const numberValue = findElementShallow<NumberValue>(child, "Value");
+				return numberValue.Value === idRoom;
+			});
+
+		if (!roomFolder) {
+			print("Warning not room found with that id");
+		}
+
+		//TODO: Improve, modulate the code
+		const matchFolder = findElementShallow<Folder>(roomFolder as Folder, "Match");
+		const cabinetFolder = findElementShallow<Folder>(matchFolder, "Cabinet");
+		const cabinetDoor = findElementShallow<Folder>(cabinetFolder, "cabinet_door");
+		const animator = findElement<Animator>(cabinetDoor, "Animator");
+		const animation = findElement<Animation>(cabinetDoor, "Animation");
+		const track = animator.LoadAnimation(animation);
+		track.Play();
 	});
 };
--- src/shared/services/gom-service.module.ts
+++ src/shared/services/gom-service.module.ts
@@ -6,3 +6,12 @@ export const findElement = <T extends Instance>(root: Instance, nameElement: str
 
 	return element as T;
 };
+
+export const findElementShallow = <T extends Instance>(root: Instance, nameElement: string) => {
+	const element = root.FindFirstChild(nameElement);
+
+	const assertMsg = `Warning not found  ${nameElement}`;
+	assert(element, assertMsg);
+
+	return element as T;
+};
